44e460dedb7da6cd7a5f27d643322d4570519c8e
Luke Winslow <lwinslow@usgs.gov>
2014-09-15 21:21:30 -0400

    Scraped a bunch of CA reservoir data and began plotting it to see what we had.

---
+++ ca_reservoirs/R/figure_all_combined_reservoirs.R
@@ -0,0 +1,33 @@
+## Lets look at overall storage in all reservoirs
+
+Sys.glob('../storage_data/*')
+
+sites = read.csv('../Data/ca_reservoirs.csv', as.is=TRUE)
+all.data = data.frame()
+for(i in 1:nrow(sites)){
+  fname = paste0('../storage_data/', sites$ID[i], '.csv')
+  if(!file.exists(fname)){
+    next
+  }
+  
+  data = read.csv(fname, as.is=TRUE)
+  
+  if(nrow(data) < 1500){
+    next
+  }
+  names(data) = c("Date", sites$ID[i])
+  if(nrow(all.data) == 0){
+    all.data = data   
+  }else{
+    all.data = merge(all.data, data, by="Date", all=TRUE)
+  }
+}
+
+## TODO: Fix this, currently doesn't handle lakes with missing data
+all.dates = as.POSIXct(all.data$Date)
+all.vols = apply(as.matrix(all.data[,-1]), 1, sum, na.rm=TRUE)
+
+png('../storage_plots/figure_all_CA_storage.png', width=1200, height=900, res=150)
+plot(all.dates, all.vols/810713.194, ylim=c(12,38), ylab=expression(Storage~km^3), xlab='')
+dev.off()
+
---
+++ ca_reservoirs/R/get_cal_reservoir_storage.R
@@ -0,0 +1,40 @@
+library(RCurl)
+library(XML)
+
+scrape_ca_storage = function(site){
+  theurl = sprintf('http://cdec.water.ca.gov/cgi-progs/queryDaily?%s&d=%s&span=2000days', 
+                   site, '14-Sep-2014')
+  cat(theurl,'\n')
+  tables = readHTMLTable(theurl)
+  
+  ## it seems to be the only table grabbed. We must be lucky
+  data = tables[[1]]
+  data = data[,c('Date', 'STORAGE&nbsp')]
+  
+  #2nd row are units. Drop them
+  data = data[-1,]
+  names(data) = c("Date", "Storage(acre-feet)")
+  
+  data$Date = strptime(data$Date, '%m/%d/%Y')
+  data$`Storage(acre-feet)` = as.numeric(as.character(data[,2]))
+  return(data)
+}
+
+sites = read.csv('../Data/ca_reservoirs.csv', as.is=TRUE)
+
+for(i in 1:nrow(sites)){
+  
+  tryCatch({
+    data = scrape_ca_storage(sites$ID[i])
+    
+    write.table(data, paste0('../storage_data/', sites$ID[i], '.csv'), sep=',', row.names=FALSE)
+    
+    png(paste0('../storage_plots/', sites$ID[i], '.png'))
+    plot(data[,1], data[,2], main=sites$ID[i], ylab='Storage(acre-feet)', xlab='')
+    dev.off()
+  }, error=function(err){})
+}
+
+#data = scrape_ca_storage('ORO')
+
+
