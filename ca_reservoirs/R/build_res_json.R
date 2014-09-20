abc52b2a484b4ff50254ec45c918c333b54288d3
Jordan S Read <jread@usgs.gov>
2014-09-20 13:13:59 -0500

    NULLs as nulls, not NAs

--- ca_reservoirs/R/build_res_json.R
+++ ca_reservoirs/R/build_res_json.R
@@ -1,6 +1,6 @@
-#library(jsonlite)
+library(rjson)
 week_time <- seq.POSIXt(as.POSIXlt('2010-01-05'), as.POSIXlt('2014-09-20'), by = 'week')
-#week_time <- seq.POSIXt(as.POSIXlt('2014-09-05'), as.POSIXlt('2014-09-20'), by = 'week')
+
 
 sites = read.csv('../Data/ca_reservoirs.csv', as.is=TRUE)
 
@@ -13,7 +13,7 @@ for (j in 1:num_steps){
   stations <- vector('list', length=num_station)
   
   for (i in 1:num_station){
-    stations[[i]] <- list(name=sites$ID[i], storage = NA)
+    stations[[i]] <- list(name=sites$ID[i], storage = NULL)
     # downsample obs data
     period <- c(trunc(week_time[j]-3*86400, 'days'), trunc(week_time[j]+4*86400, 'days'))
     file_nm <- paste0('../storage_data/', sites$ID[i], '.csv')
@@ -23,7 +23,10 @@ for (j in 1:num_steps){
       use_i <- dates <= period[2] & dates > period[1]
       if (sum(use_i) > 0){
         val <- mean(dat[use_i, 2], na.rm = TRUE)
-        stations[[i]]$storage <- ifelse(is.na(val),NA,val)
+        if (!is.na(val)){
+          stations[[i]]$storage <- val
+        }
+        
       }
       
     }
@@ -34,7 +37,8 @@ for (j in 1:num_steps){
   time_out[[j]] <- list('time'=format(trunc(week_time[j], 'days')), 'station'=stations)
   
 }
-library(rjson)
+
 json <- toJSON(time_out)
+
 cat(json,file = '../storage_data/storage.json')
 
