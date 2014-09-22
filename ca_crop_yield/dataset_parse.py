5aee1f1835bb0a253c0121e0c057ba1b4af43095
Yan, Andrew N. <ayan@usgs.gov>
2014-09-22 09:11:41 -0500

    added function to find csv row number containing pertinent string

--- ca_crop_yield/dataset_parse.py
+++ ca_crop_yield/dataset_parse.py
@@ -11,7 +11,7 @@ Created on Sep 16, 2014
 # economic damage
 
 # 2001 - Present
-
+import csv
 import pandas as pd
 from matplotlib.pylab import plt
 
@@ -28,21 +28,43 @@ def price_per_ton(series, production_series, value_series):
     production = series[production_series]
     price_per_production = value / production
     return price_per_production
+
+
+def get_csv_row_number(csv_file, search_string, delimiter=','):
+    iterator = 0
+    row_number = None
+    with open(csv_file, 'rt') as cf:
+        reader = csv.reader(cf, delimiter=delimiter)
+        for row in reader:
+            iterator += 1
+            for field in row:
+                if field == search_string:
+                    row_number = iterator
+    header_row = row_number - 1
+    return (row_number, header_row)
+                    
+    
         
-
-csv_path = 'data/Table-B22.txt' # olive production data
-df = pd.read_csv(csv_path, sep='\t', quotechar='"', skiprows=8, header=None)
-
-df1 = df[[0, 2, 12]]
-
-df1['year'] = df[0]
-df1['total_production'] = df[2]
-df1['total_value'] = df[12]
-df2 = df1[['year', 'total_production', 'total_value']]
-df3 = df2.applymap(convert_to_float)
-df3_no_nans = df3[pd.notnull(df3['year'])]
-df3_no_nans['price_per_production'] = df3_no_nans.apply(price_per_ton, axis=1, production_series='total_production', value_series='total_value')
-df3_no_nans.plot(x='year', y='price_per_production', kind='scatter')
-plt.ylabel('Total Value of Olives')
-plt.xlabel('Total Production of Olives')
-plt.show()
+if __name__ == '__main__':
+    """
+    csv_path = 'data/Table-B22.txt' # olive production data
+    df = pd.read_csv(csv_path, sep='\t', quotechar='"', skiprows=8, header=None)
+    
+    df1 = df[[0, 2, 12]]
+    
+    df1['year'] = df[0]
+    df1['total_production'] = df[2]
+    df1['total_value'] = df[12]
+    df2 = df1[['year', 'total_production', 'total_value']]
+    df3 = df2.applymap(convert_to_float)
+    df3_no_nans = df3[pd.notnull(df3['year'])]
+    df3_no_nans['price_per_production'] = df3_no_nans.apply(price_per_ton, axis=1, production_series='total_production', value_series='total_value')
+    df3_no_nans.plot(x='year', y='price_per_production', kind='scatter')
+    plt.ylabel('Total Value of Olives')
+    plt.xlabel('Total Production of Olives')
+    plt.show()
+    """
+    
+    csv_path = 'C:\\Users\\ayan\\git\\CIDA-Viz\\ca_crop_yield\\data\\emp_data_2008_2010\\ca2008emp.csv'
+    search_str_row_number = get_csv_row_number(csv_path, 'TOTAL AGRICULTURAL')
+    print(search_str_row_number)
--- ca_crop_yield/emp_data_analysis.py
+++ ca_crop_yield/emp_data_analysis.py
@@ -6,14 +6,16 @@ Created on Sep 21, 2014
 
 import pandas as pd
 from excel_to_csv import get_files_in_directory
+from dataset_parse import get_csv_row_number
 
-emp_data_dir_2013 = 'data/emp_data_2013'
+emp_data_dir_2013 = 'data/emp_data_2008_2010'
 emp_2013_files = get_files_in_directory(emp_data_dir_2013, '.csv')
 df_2013 = []
 print(emp_2013_files)
 for csv_file in emp_2013_files:
     pathname = csv_file[0]
-    df = pd.read_csv(pathname, sep=',', quotechar='"', skiprows=3, index_col=1, header=0)
+    ag_row, header_row = get_csv_row_number(pathname, 'TOTAL AGRICULTURAL')
+    df = pd.read_csv(pathname, sep=',', quotechar='"', skiprows=header_row - 1, index_col=1, header=header_row)
     df_2013.append(df)
 print(df_2013[0])
 for df in df_2013:
