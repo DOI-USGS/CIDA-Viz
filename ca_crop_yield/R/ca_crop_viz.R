f68912ca973f40e3b6353336c37e543f2f516e89
Jordan home <jread@usgs.gov>
2014-09-26 20:46:05 -0500

    crop yield interactive plot.ly

--- ca_crop_yield/R/ca_crop_viz.R
+++ ca_crop_yield/R/ca_crop_viz.R
@@ -1,26 +1,11 @@
-ca_crop_viz <- function(username,key){
+ca_crop_viz <- function(username,key,plot_names,plot_colors){
   library(plotly)
   py <- plotly(username=username, key=key)
   
-  trace1 <- list(
-    x = c(0.7286253 ,1.619379, 2.92937, 3.424403),
-    y = c( 1.268, 1.283, 1.377, 1.467), 
-    mode = "markers", 
-    name = "Oranges", 
-    text = as.character(seq(2011, 2014)),
-    marker = list(
-      color = "rgb(255, 153, 0)", 
-      size = 12, 
-      line = list(
-        color = "white", 
-        width = 0.5
-      )
-    ), 
-    type = "scatter"
-  )
-  data <- list(trace1)
+
+  data <- build_list(plot_names,plot_colors)
   layout <- list(
-    title = "Agricultural output cost data", 
+    title = "Agricultural output cost data (August)", 
     xaxis = list(
       title = "Statewide drought index (0-4)", 
       showgrid = FALSE, 
@@ -35,3 +20,36 @@ ca_crop_viz <- function(username,key){
   url <- response$url
   return(url)
 }
+
+build_list <- function(plot_names,plot_colors){
+  
+  plot_list <- vector('list', length=length(plot_names))
+  
+  for (i in 1:length(plot_names)){
+    crop_name <- plot_names[i]
+    y_vals <- get_crop_nums(crop_name,seq(2011,2014))
+    plot_list[[i]] <- list(
+      x = c(0.7286253 ,1.619379, 2.92937, 3.424403),
+      y = y_vals, 
+      mode = "markers", 
+      name = paste(toupper(substring(crop_name, 1,1)), substring(crop_name, 2),
+                   sep="", collapse=" "),
+      text = as.character(seq(2011, 2014)),
+      marker = list(
+        color = plot_colors[i], 
+        size = 18, 
+        line = list(
+          color = "white", 
+          width = 0.5,
+          size = 18
+        )
+      ), 
+      type = "scatter"
+    )
+  }
+  return(plot_list)
+}
+plot_names = c('oranges','lemons','grapes','lettuce')
+plot_colors = c("rgb(255, 153, 0)", "rgb(255, 255, 150)", "rgb(68, 28, 82)", "rgb(0, 255, 128)")
+
+ca_crop_viz('j$$$','$$$',plot_names,plot_colors)
