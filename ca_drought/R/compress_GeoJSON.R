1def8b80f47402408ced0be5af482dc4a8191c0d
Jordan S Read <jread@usgs.gov>
2014-10-04 17:00:39 -0500

    map compress

--- ca_drought/R/compress_GeoJSON.R
+++ ca_drought/R/compress_GeoJSON.R
@@ -3,8 +3,9 @@ library('rgeos')
 library('rgdal')
 library('maps')
 compress_gjson <- function(time_st){
-  map = readOGR(drought_json, "OGRGeoJSON")
+  
   drought_json = paste0('../../Vizzies/public_html/data/drought_shp/USDM_',time_st,'.json')
+  map = readOGR(drought_json, "OGRGeoJSON")
   drought_low <- paste0('../data/USDM_',time_st,'_low.json')
   
   map_low <- gSimplify(map,tol = .03, topologyPreserve=TRUE) ## simplify
@@ -13,7 +14,8 @@ compress_gjson <- function(time_st){
                                   data=as.data.frame(slot(map, "data")))
   
   
-  writeOGR(obj = spp,dsn = drought_low, layer = 'drought',driver = 'GeoJSON')
+  tryCatch({writeOGR(obj = spp,dsn = drought_low, layer = 'drought',driver = 'GeoJSON')
+  }, error=function(err){})
 }
 
 
