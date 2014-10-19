c42e060afb8a9a7ffaeb89b62d264eb5bf87d571
Jordan Walker <jiwalker@usgs.gov>
2014-10-19 13:24:17 -0500

    fixed up landsat section, also removing reservoir sites when leave section

--- Vizzies/public_html/js/main.js
+++ Vizzies/public_html/js/main.js
@@ -150,7 +150,7 @@ $(document).ready(function () {
 	// build scenes
 	new ScrollScene({triggerElement: "#start-trigger", duration: $(window).height()})
 		.on("enter", function (e) {
-			//panAndZoom(continentalCenter, continentalZoom);
+			panAndZoom(continentalCenter, continentalZoom);
 			map.replaceLayer(getInitialDroughtLayer(), 'drought');
 			activateAnchorLink(1);
 		})
@@ -162,10 +162,10 @@ $(document).ready(function () {
 		.setPin("#drilling-pin")
 		.addTo(controller);
 	// Scene 1 (reservoirs) built in response to ajax
-	new ScrollScene({triggerElement: "#drought2014-trigger", duration: 5000})
-		.setPin("#drought2014-pin")
+	new ScrollScene({triggerElement: "#landsat-trigger", duration: 5000})
+		.setPin("#landsat-pin")
 		.on("enter", function (e) {
-			panAndZoom(caliCenterCenter, caliZoom);
+			panAndZoom(caliRightCenter, caliZoom);
 			activateAnchorLink(4);
 		})
 		.addTo(controller);
@@ -203,7 +203,7 @@ $(document).ready(function () {
 		.addTo(controller);
 	new ScrollScene({triggerElement: "#credits-trigger", duration: $(window).height()})
 		.on("enter", function (e) {
-			panAndZoom(caliRightCenter, caliZoom);
+			panAndZoom(continentalCenter, continentalZoom);
 			map.replaceLayer(getInitialDroughtLayer(), 'drought');
 			activateAnchorLink(7);
 		})
@@ -245,6 +245,22 @@ $(document).ready(function () {
 			
 			if (layer.getSource().getState() === 'ready') {
 				layer.getSource().dispatchChangeEvent();
+			}
+		}
+	};
+	
+	map.removeLayerOfType = function(layerType) {
+		if (layerType) {
+			var layers = map.getLayers().getArray().filter(function (oldLayer) {
+				if (oldLayer) {
+					return oldLayer.layer_type === layerType;
+				} else {
+					return false;
+				}
+			});
+
+			for (var i = 0; i < layers.length; i++) {
+				map.removeLayer(layers[i]);
 			}
 		}
 	};
@@ -314,6 +330,7 @@ $(document).ready(function () {
 		.on("leave", function (e) {
 			$("#res-plot-container").hide();
 			$("#time-indicator").hide();
+			map.removeLayerOfType("reservoir");
 		})
 		.addTo(controller);
 
