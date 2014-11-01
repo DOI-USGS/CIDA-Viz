5d2eade2c2c3ec28bae032cfacdd009627d53fb0
Jordan S Read <jread@usgs.gov>
2014-11-01 15:25:41 -0500

    linked points

--- ca_discharge/createSVG.R
+++ ca_discharge/createSVG.R
@@ -13,8 +13,21 @@ add_CA <- function(g_id){
                                         d="M 157.261719 145.480469 L 149.464844 150.140625 L 148.332031 151.480469 L 146.953125 156.382812 L 148.261719 161.128906 L 151.523438 165.761719 L 153.320312 169.929688 L 152.585938 178.488281 L 155.949219 184.621094 L 154.96875 188.644531 L 154.515625 189.832031 L 139.824219 191.402344 L 131.957031 160.921875 L 125.769531 152.257812 L 124.320312 147.902344 L 123.816406 141.390625 L 124.3125 137.238281 L 126 132.242188 L 129.527344 127.351562 L 135.167969 124.078125 Z "))
   pth_7 <- newXMLNode("path", attrs = c(style="fill-rule:nonzero;fill:rgb(100%,100%,26.666667%);fill-opacity:0.588235;stroke-width:0.75;stroke-linecap:round;stroke-linejoin:round;stroke:rgb(30.196078%,30.196078%,30.196078%);stroke-opacity:1;stroke-miterlimit:10;",
                                         d="M 154.515625 189.832031 L 155.949219 184.621094 L 152.585938 178.488281 L 153.320312 169.929688 L 151.523438 165.761719 L 148.261719 161.128906 L 146.953125 156.382812 L 148.332031 151.480469 L 149.464844 150.140625 L 157.261719 145.480469 L 160.691406 148.800781 L 160.679688 151.109375 L 163.023438 154.003906 L 164.222656 158.542969 L 167.878906 162.039062 L 163.542969 165.183594 L 162.089844 167.9375 L 162.253906 174.753906 L 159.378906 177.40625 L 159.644531 183.082031 L 163.050781 185.167969 L 162.21875 189.011719 Z "))
-
-    g_id <- addChildren(g_id,c(pth_1, pth_2, pth_3, pth_4, pth_5, pth_6, pth_7))  
+  pth_8 <- newXMLNode("circle", attrs = c(cx="140", cy="178.113281", r = "5",style = "fill:rgb(40%,40%,40%); fill-opacity: 0.2"))
+  set_1 <- newXMLNode('set', attrs = c(attributeName="r", to="15", begin="nwis_11097500.mouseover",  end="nwis_11097500.mouseout"))
+  set_2 <- newXMLNode('set', attrs = c(attributeName="fill-opacity", to="1", begin="nwis_11097500.mouseover",  end="nwis_11097500.mouseout"))
+  pth_8 <- addChildren(pth_8, c(set_1, set_2))
+  pth_9 <- newXMLNode("circle", attrs = c(cx="130", cy="158.113281", r = "5",style = "fill:rgb(40%,40%,40%); fill-opacity: 0.2"))
+  set_1 <- newXMLNode('set', attrs = c(attributeName="r", to="15", begin="nwis_11093000.mouseover",  end="nwis_11093000.mouseout"))
+  set_2 <- newXMLNode('set', attrs = c(attributeName="fill-opacity", to="1", begin="nwis_11093000.mouseover",  end="nwis_11093000.mouseout"))
+  pth_9 <- addChildren(pth_9, c(set_1, set_2))
+  pth_10 <- newXMLNode("circle", attrs = c(cx="110", cy="118.113281", r = "5",style = "fill:rgb(40%,40%,40%); fill-opacity: 0.2"))
+  set_1 <- newXMLNode('set', attrs = c(attributeName="r", to="15", begin="nwis_11086500.mouseover",  end="nwis_11086500.mouseout"))
+  set_2 <- newXMLNode('set', attrs = c(attributeName="fill-opacity", to="1", begin="nwis_11086500.mouseover",  end="nwis_11086500.mouseout"))
+  pth_10 <- addChildren(pth_10, c(set_1, set_2))
+  
+  
+    g_id <- addChildren(g_id,c(pth_1, pth_2, pth_3, pth_4, pth_5, pth_6, pth_7, pth_8, pth_9, pth_10))  
   return(g_id)
 }
 
@@ -90,7 +103,7 @@ createSVG <- function(points, file_nm){
   for (i in 1:length(points[[1]])){
     
     mouse_move_txt <- paste0("ShowTooltip(evt, '", points$text[[i]], "')")
-    pnt <- newXMLNode("circle", parent=g_id, attrs = c(cx=points$cx[[i]], cy=points$cy[[i]],
+    pnt <- newXMLNode("circle", parent=g_id, attrs = c(id = points$id[[i]], cx=points$cx[[i]], cy=points$cy[[i]],
                                                        r=points$r[[i]], fill="#4169E1", opacity=def_opacity,
                                                        onmouseover="MakeOpaque(evt)",
                                                        onmousemove=mouse_move_txt,
