4ab873b645a47335838fdafa0e34acae488ccb7f
Jordan S Read <jread@usgs.gov>
2014-11-02 11:32:27 -0600

    scaling

--- ca_discharge/R/get_CA_paths.R
+++ ca_discharge/R/get_CA_paths.R
@@ -9,11 +9,19 @@ get_CA_paths <- function(g_id, lyr_info, x_crd, y_crd){
   
   # just a little check...
   if (!all((names(xmlAttrs(xmltop[[1]][[1]])[3:4]) == c('width','height')))){stop('bad data')}
-  doc_width <- xmlAttrs(xmltop[[1]][[1]])[[3]]
-  doc_height <- xmlAttrs(xmltop[[1]][[1]])[[4]]
+  doc_width <- as.numeric(xmlAttrs(xmltop[[1]][[1]])[[3]])
+  doc_height <- as.numeric(xmlAttrs(xmltop[[1]][[1]])[[4]])
+  
+  scale_f <- diff(x_crd)/doc_width #(ASSUMING SQUARE)!!!
+  trn_x <- x_crd[1]/scale_f
+  trn_y <- y_crd[1]/scale_f
+  
+  
+  
   paths <- xmltop[[1]][names((xmltop[[1]])) == 'path']
   for (i in 1:length(paths)){
-    paths[[i]] <- addAttributes(paths[[i]], 'transform'="scale(0.75)translate(25,25)")
+    paths[[i]] <- addAttributes(paths[[i]], 'transform'=paste0(
+      "scale(",as.character(scale_f) ,")translate(",as.character(trn_x),",",as.character(trn_y),")"))
   }
   g_id <- addChildren(g_id, paths)
   
