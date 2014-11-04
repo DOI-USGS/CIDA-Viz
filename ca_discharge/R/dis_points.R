04d58e29b007baaf443de45da7251360165a5287
Jordan S Read <jread@usgs.gov>
2014-11-03 19:39:36 -0600

    removed log fau function

--- ca_discharge/R/dis_points.R
+++ ca_discharge/R/dis_points.R
@@ -5,16 +5,11 @@ dis_points <- function(){
   
   points <- data.frame(meanDis = dat$meanDis, todayDis = dat$todayDis, 
                        r = rep(radius, nrow(dat)), text = dat$siteName, id = dat$STAID,
-                       sitex = dat$lon, sitey = dat$lat)
+                       sitex = dat$lon, sitey = dat$lat, 
+                       p_10 = dat$percent10, p_25 = dat$percent25, p_75 = dat$percent75, p_90 = dat$percent90)
   rmv_i <- is.na(points$meanDis)
   
   points <- points[!rmv_i, ]
   
   return(points)
-}
-
-transform_pts <- function(x, y){
-  x <- log(x)*25+378
-  y <- 150-log(y)*25
-  return(data.frame(x,y))
 }
